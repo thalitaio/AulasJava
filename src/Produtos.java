b3a82f4cc607d1dda9793104d680b563a25b2a2f
thcarval <contatothalitanunes@gmail.com>
2021-09-13 16:52:30 -0300

    update

--- src/Produtos.java
+++ src/Produtos.java
@@ -7,13 +7,13 @@ public class Produtos {
         System.out.println("================== Cadastro Produtos ============");
         System.out.println("Bem-vindo. Digite as informacoes do solicitadas.");
         System.out.print("Digite o nome do produto: ");
-        String nome = sc.next();
+        String nome = sc.nextLine();
         System.out.print("Digite uma breve descricao do produto: ");
-        String descricao = sc.next();
+        String descricao = sc.nextLine();
         System.out.print("Digite o valor do produto: ");
         double valor = sc.nextDouble();
         System.out.print("Digite a categoria do produto: ");
-        String categoria = sc.next();
+        String categoria = sc.nextLine();
 
         System.out.printf("Produto cadastrado com sucesso. Produto: %s -- Descricao: %s -- Valor: %f -- Categoria: %s", nome, descricao, valor, categoria);
 
