78620dedb40462d8217f8063bd33668f83bb0630
thcarval <contatothalitanunes@gmail.com>
2021-09-14 11:47:56 -0300

    update idade com sobrenome

--- src/Idade.java
+++ src/Idade.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Idade {
+import java.util.Scanner;
+
+public class Idade {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("================== Cadastro ============");
+        System.out.print("Digite o seu nome: ");
+        String nome = sc.next();
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = sc.next();
+        System.out.print("Digite a sua idade: ");
+        int idade = sc.nextInt();
+        if (idade >= 18){
+            System.out.printf("Parabens, %s %s. voce tem %d anos e pode se cadastrar.", nome, sobrenome, idade);
+        } else {
+            System.out.printf("Lamentamos, %s %s. Voce tem %d anos e nao pode se cadastrar.", nome, sobrenome, idade);
+        }
+    }
 }
