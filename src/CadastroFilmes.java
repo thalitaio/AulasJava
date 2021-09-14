6da1f6f44b608c5648f4432e319497758de645bc
thcarval <contatothalitanunes@gmail.com>
2021-09-14 14:36:41 -0300

    update

--- src/CadastroFilmes.java
+++ src/CadastroFilmes.java
@@ -5,11 +5,12 @@ public class CadastroFilmes {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("================== Cadastro de Filmes ============");
+        System.out.println("================== Cadastro de Filmes, Series e Documentarios ================= ");
+        System.out.println("================== Digite o numero correspondente a opcao desejada =================");
         System.out.println("1 - Cadastrar filme ");
         System.out.println("2 - Cadastrar serie ");
         System.out.println("3 - Cadastrar documentario ");
-        System.out.println("4 - Sair /n");
+        System.out.println("4 - Sair ");
 
         int escolha = sc.nextInt();
         switch (escolha){
@@ -22,7 +23,7 @@ public class CadastroFilmes {
                 String genero = sc.next();
                 System.out.println("Digite o ano do filme: ");
                 int ano = sc.nextInt();
-                System.out.printf("Parabens, o filme %s foi cadastrado com sucesso" ,nome);
+                System.out.printf("Parabens, o filme %s foi cadastrado com sucesso!" ,nome);
                 break;
             case 2:
                 System.out.println("Digite o nome da serie: ");
@@ -31,7 +32,7 @@ public class CadastroFilmes {
                 int anoserie = sc.nextInt();
                 System.out.println("Digite a produtora da serie: ");
                 String produtora = sc.next();
-                System.out.printf("Parabens, a serie %s foi cadastrada com sucesso" ,nomeserie);
+                System.out.printf("Parabens, a serie %s foi cadastrada com sucesso!" ,nomeserie);
                 break;
             case 3:
                 System.out.println("Digite o nome do documentario: ");
@@ -40,10 +41,10 @@ public class CadastroFilmes {
                 int anodoc = sc.nextInt();
                 System.out.println("Digite o tema do documentario: ");
                 String tema = sc.next();
-                System.out.printf("Parabens, o documentario %s foi cadastrado com sucesso" ,nomedoc);
+                System.out.printf("Parabens, o documentario %s foi cadastrado com sucesso!" ,nomedoc);
                 break;
             case 4:
-                System.out.println("Fim do programa");
+                System.out.println("Fim do programa.");
                 break;
             default:
                 System.out.println("Opcao invalida. ");
